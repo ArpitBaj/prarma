64e804c399930a081aed39b8721b592cfaf234f1
Arpit Baj <arpitbaj21@gmail.com>
2023-05-01 17:17:36 +0530

    deploy
    deploy

---
+++ script.js
@@ -0,0 +1,24 @@
+
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('<span style="color:#b2ce32;">WE ARE<span\>')
+.pauseFor(300)
+.deleteAll()
+.pauseFor(300)
+.typeString('<span style = "color:#005caa;">AQU<span\><span style="color:#b2ce32;">TRUS<span\>')
+.pauseFor(5000)
+
+
+
+.start()
+
+
+
+
+
+    
+  
